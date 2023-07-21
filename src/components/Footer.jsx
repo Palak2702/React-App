8a5835a752a62db4aade01e01de1a8147b11a9cf
Palak khandelwal <khandelwalpalak175@gmail.com>
2023-07-21 10:08:43 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+  return <footer>
+    <div>
+        <h1>Drackstar</h1>
+        <p>@all right reserved</p>
+    </div>
+    <div>
+        <h5>Follow</h5>
+        <div>
+            <a href="http://github.com/Palak2702" target={'blank'}>Github</a>
+        </div>
+    </div>
+    
+  </footer>
+}
+
+export default Footer
